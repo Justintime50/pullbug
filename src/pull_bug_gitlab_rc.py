41b1ae7580bc8bc147681025595239fb9137a06d
Justintime50 <justinpaulhammond@gmail.com>
2020-04-03 17:17:35 -0600

    RC script, Dockerfile, added variables

--- src/pull_bug_gitlab_rc.py
+++ src/pull_bug_gitlab_rc.py
@@ -1,44 +1,24 @@
 """Pull Bug is great at bugging you to merge or close your pull/merge requests."""
 import os
-import logging
 import requests
 from dotenv import load_dotenv
-import slack
 
 # Setup variables
 load_dotenv()
 AUTH = os.getenv("GITLAB_API_KEY")
 SCOPE = "all"
 STATE = "opened"
-SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN")
-SLACK_CLIENT = slack.WebClient(SLACK_BOT_TOKEN)
 GITLAB_API_URL = os.getenv("GITLAB_API_URL")
+ROCKET_CHAT_URL = os.getenv("ROCKET_CHAT_URL")
 
 # Setup endpoint
 HEADERS = {'Authorization': f'Bearer {AUTH}'}
 RESPONSE = requests.get(f"{GITLAB_API_URL}/merge_requests?scope={SCOPE}&state={STATE}", headers=HEADERS)
 
-def send_message(slack_client, msg):
-    """Send Slack messages via a bot"""
-    logging.debug("Authorized Slack Client")
-    logging.basicConfig(level=logging.DEBUG)
-
-    # Make the POST request through the python slack client
-    slack_response = slack_client.chat_postMessage(
-        channel=os.getenv("SLACK_CHANNEL"),
-        text=msg
-    )
-
-    # Check if the request was a success
-    if slack_response['ok'] is not True:
-        logging.error(slack_response)
-    else:
-        logging.debug(slack_response)
-
 # Iterate over each merge request
 DATA = RESPONSE.json()
 i = 0
-send_message(SLACK_CLIENT, ":bug: *The following merge requests on GitLab are still open and need your help!*\n")
+requests.post(ROCKET_CHAT_URL, data={'text':":bug: *The following merge requests on GitLab are still open and need your help!*\n"})
 for stale_request in DATA:
     if DATA[i]['assignee'] is None:
         user = "No assignee"
@@ -49,7 +29,7 @@ for stale_request in DATA:
     message = f"*Merge Request:* <{DATA[i]['web_url']}|{DATA[i]['title']}>\n*Description:* {DATA[i]['description']}\n*Waiting on:* {user}\n"
     # Send Slack message
     try:
-        send_message(SLACK_CLIENT, message)
+        CHAT_ENDPOINT = requests.post(ROCKET_CHAT_URL, data={'text':message})
     except IndexError:
-        send_message(SLACK_CLIENT, "No merge requests stale today! Nice job.")
+        requests.post(ROCKET_CHAT_URL, data={'text':'No merge requests stale today! Nice job.'})
     i += 1
