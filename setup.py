73ae89b8887982b876435a307dd26b8f1128451a
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2021-05-31 18:39:43 -0600

    v2.3.0 - pin dependencies

--- setup.py
+++ setup.py
@@ -1,17 +1,25 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 REQUIREMENTS = [
-    'requests >= 1.0.0',
-    'slackclient >= 2.0.1',
-    'python-dotenv >= 0.10.0'
+    'requests == 2.*',
+    'slackclient == 2.*',
+    'python-dotenv == 0.17.*'
+]
+
+DEV_REQUIREMENTS = [
+    'coveralls == 3.*',
+    'flake8',
+    'mock == 4.*',
+    'pytest == 6.*',
+    'pytest-cov == 2.*',
 ]
 
 setuptools.setup(
     name='pullbug',
-    version='2.2.0',
+    version='2.3.0',
     description='Get bugged via Discord, Slack, or RocketChat to merge your GitHub pull requests or GitLab merge requests.',  # noqa
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -26,13 +34,7 @@ setuptools.setup(
     ],
     install_requires=REQUIREMENTS,
     extras_require={
-        'dev': [
-            'pytest >= 6.0.0',
-            'pytest-cov >= 2.10.0',
-            'coveralls >= 2.1.2',
-            'flake8 >= 3.8.0',
-            'mock >= 4.0.0',
-        ]
+        'dev': DEV_REQUIREMENTS
     },
     entry_points={
         'console_scripts': [
