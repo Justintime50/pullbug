111193a21c7f11859a4d8b5d00dd53b2b3de5fde
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2022-05-15 18:29:21 -0600

    chore: ignore test dir in build

--- setup.py
+++ setup.py
@@ -33,8 +33,17 @@ setuptools.setup(
     url='http://github.com/justintime50/pullbug',
     author='Justintime50',
     license='MIT',
-    packages=setuptools.find_packages(),
-    package_data={'pullbug': ['py.typed']},
+    packages=setuptools.find_packages(
+        exclude=[
+            'examples',
+            'test',
+        ]
+    ),
+    package_data={
+        'pullbug': [
+            'py.typed',
+        ]
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
